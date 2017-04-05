ba92d98f31e27630f3407b6a3bfaef06631e5ad5
heyjulieh <heyjulieh@gmail.com>
2017-04-05 09:59:06 -0700

    final draft of hw-- loads images, did not do bonus, does not refresh images

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,43 @@
 $(document).on("ready", function(){
 
+  $("form").on("submit", function(e) {
+     e.preventDefault();
+     showGifGallery();
+  });
 });
+function showGifGallery(){
+  $.ajax({
+  	// What kind of request
+  	method: "GET",
+
+  	// The URL for the request
+  	url: "http://api.giphy.com/v1/gifs/trending",
+
+  	// The data to send aka query parameters
+  	datatype: 'json',
+
+    data: $("form").serialize(),
+
+  	// Code to run if the request succeeds;
+  	// the response is passed to the function
+  	success: onSuccess,
+
+  	// Code to run if the request fails; the raw request and
+  	// status codes are passed to the function
+  	error: onError
+  });
+}
+
+function onSuccess(results) {
+$(".gif-img").remove();
+results.data.forEach(function(k,i){
+  $(".gif-gallery").append($("<img src="+k.images.fixed_height.url+">"));
+});
+}
+
+  function onError(xhr, status, errorThrown) {
+  	alert("Sorry, there was a problem!");
+  	console.log("Error: " + errorThrown);
+  	console.log("Status: " + status);
+  	console.dir(xhr);
+  }
